1251a0170ee0720f274273164b40e60f0bde3bac
tokeichun <92981211+yvenone@users.noreply.github.com>
2022-05-07 12:22:54 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+button, input {
+    padding: 5px 20px;
+    margin: 5px;
+}
+
+select{
+    padding: 5px;
+    margin-top: 10px;
+    width: 22%; 
+}
